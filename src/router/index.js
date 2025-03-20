8ae05f7b8117e4bd519a6234a88baed4ef29dbd9
Milarize <mailmint085#gmail.com>
2025-03-20 20:04:13 +0700

    Fix formatting in vite.config.js by adding a newline at the end of the file.

---
+++ src/router/index.js
@@ -0,0 +1,10 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHashHistory('/myprofile/'),
+  routes: [
+
+  ]
+})
+
+export default router 
